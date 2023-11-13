1222cf52ca3eaa963ce891e26a9be7bc02b63360
christiansakerb <csaker95@gmail.com>
2023-11-12 20:29:25 -0500

    Borramos dependencias que no funcionan

--- Modelo.py
+++ Modelo.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import seaborn as sns
 import warnings
 from imblearn.over_sampling import SMOTE
 from sklearn.model_selection import train_test_split
