7f47a8e462cbf47531f0ee7165cb7a0bd455f44a
christiansakerb <csaker95@gmail.com>
2023-11-22 21:28:57 -0500

    Tablero funcional en dash version 1.1

--- dashboard_dash.py
+++ dashboard_dash.py
@@ -2,7 +2,7 @@ from dash import Dash, html, dcc, callback, Output, Input
 import plotly.express as px
 import pandas as pd
 
-df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder_unfiltered.csv')
+df = pd.read_csv('Archivo_para_tabler.csv')
 
 app = Dash(__name__)
 
@@ -11,8 +11,8 @@ app = Dash(__name__)
 #Dropwdown es una lista desplegable, se define valor incial
 #Graph es un grafico, en este caso cambiante según callback
 app.layout = html.Div([
-    html.H1(children='Title of Dash App', style={'textAlign':'center'}),
-    dcc.Dropdown(df.country.unique(), 'Canada', id='dropdown-selection'),
+    html.H1(children='Dashboard Ciclo de Vida de Productos', style={'textAlign':'center'}),
+    dcc.Dropdown(df.CALIFICACION.unique(), 'ESTRELLA', id='dropdown-selection'),
     dcc.Graph(id='graph-content')
 ])
 
@@ -23,8 +23,8 @@ app.layout = html.Div([
     Input('dropdown-selection', 'value')
 )
 def update_graph(value):
-    dff = df[df.country==value]
-    return px.line(dff, x='year', y='pop')
+    dff = df[df.CALIFICACION==value]
+    return px.line(dff, x='Semana de Fecha', y='CONTRIBUCION')
 
 if __name__ == '__main__':
     app.run(debug=True)
