aad670c2a0de24b48791799b7593d94499e62dba
christiansakerb <csaker95@gmail.com>
2023-11-25 18:05:13 -0500

    ajutes funciones de pipeline

--- modelo_clasificacion/model/pipeline.py
+++ modelo_clasificacion/model/pipeline.py
@@ -1,37 +1,12 @@
-from feature_engine.selection import DropFeatures
 from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import MinMaxScaler
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.multioutput import MultiOutputClassifier
 
 from model.config.core import config
 from model.processing import features as pp
+## PIPELINE COMPLETO
 
-abandono_pipe = Pipeline(
-    [
-        # Drop features 
-        #("drop_features", 
-        # DropFeatures(
-        #     features_to_drop=[config.model_config.temp_features]
-        #     )
-        #),
-        # Mappers
-        #(
-        #    "mapper_qual",
-        #    pp.Mapper(
-        #        variables=config.model_config.qual_vars,
-        #        mappings=config.model_config.qual_mappings,
-        #    ),
-        #),
-        # Scaler
-        ("scaler", MinMaxScaler()
-         ),
-        # Random forest 
-        ("Random Forest",
-            RandomForestClassifier(
-                n_estimators = config.model_config.n_estimators, 
-                max_depth = config.model_config.max_depth,
-                random_state=config.model_config.random_state,
-            ),
-        ),
-    ]
-)
+matriz_pipeline = Pipeline([("Random Forest",MultiOutputClassifier(
+    RandomForestClassifier(max_features=config.model_config.max_features,
+                           n_estimators=config.model_config.n_estimators,
+                           random_state=config.model_config.random_state,)),),])
--- modelo_clasificacion/model/train_pipeline.py
+++ modelo_clasificacion/model/train_pipeline.py
@@ -1,33 +1,52 @@
 import numpy as np
 from config.core import config
-from pipeline import abandono_pipe
+import pandas as pd
+from pipeline import matriz_pipeline
+import SMOTE
 from processing.data_manager import load_dataset, save_pipeline
 from sklearn.model_selection import train_test_split
 
 
 def run_training() -> None:
     """Train the model."""
-
+    
     # read training data
-    data = load_dataset(file_name=config.app_config.train_data_file)
+    Ventas_Procesadas = load_dataset(file_name=config.app_config.train_data_file)
 
-    # divide train and test
-    X_train, X_test, y_train, y_test = train_test_split(
-        data[config.model_config.features],  # predictors
-        data[config.model_config.target],
-        test_size=config.model_config.test_size,
-        # we are setting the random seed here
-        # for reproducibility
-        random_state=config.model_config.random_state,
-    )
-    #y_train = np.log(y_train)
-    y_train = y_train.map(config.model_config.qual_mappings)
+    Ventas_Procesadas['Semana de Fecha'] = Ventas_Procesadas['Semana de Fecha'].apply(lambda x: int(x[-2:]))
+    Ventas_Procesadas = Ventas_Procesadas.sort_values(by='Semana de Fecha',ascending=True)
+    Ventas_Procesadas.head()
+
+    Diccionario_semanas_transformacion = {}
+
+    for mes in Ventas_Procesadas['FECHA ASIGNADO'].unique():
+        Auxiliar = Ventas_Procesadas[Ventas_Procesadas['FECHA ASIGNADO']==mes]
+        Semanas = Auxiliar['Semana de Fecha'].unique()
+        Semana_nueva = np.arange(0,Semanas.shape[0],1)
+        for indice in Semana_nueva:
+            Diccionario_semanas_transformacion[Semanas[indice]]=Semana_nueva[indice]+1
+    Ventas_Procesadas['Semana de Fecha'] = Ventas_Procesadas['Semana de Fecha'].apply(lambda x: Diccionario_semanas_transformacion[x])
+
+    Ventas_Procesadas = Ventas_Procesadas.dropna()
+
+    Ventas_Procesadas = pd.get_dummies(Ventas_Procesadas,columns=['FECHA ASIGNADO','CATEGORIA','LINEA'],drop_first=True,dtype=float)
+    Ventas_Procesadas = pd.get_dummies(Ventas_Procesadas,columns=['CALIFICACION'],dtype=float)
+
+    X =Ventas_Procesadas[config.model_config.features]
+    y = Ventas_Procesadas[config.model_config.target]
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=config.model_config.test_size, random_state=42)
+
+    sm = SMOTE()  #Smote con parámetros por default
+    smote = SMOTE(random_state=42)
+
+    X_train_resampled, y_train_resampled = smote.fit_resample(X_train.to_numpy(), y_train.to_numpy())
 
     # fit model
-    abandono_pipe.fit(X_train, y_train)
+    matriz_pipeline.fit(X_train_resampled, y_train_resampled,config.model_config.random_state)
 
     # persist trained model
-    save_pipeline(pipeline_to_persist=abandono_pipe)
+    save_pipeline(pipeline_to_persist=matriz_pipeline)
 
 
 if __name__ == "__main__":
