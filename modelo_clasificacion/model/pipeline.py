87613425f8ee9eedb76d9fd8953d564ea5619713
christiansakerb <csaker95@gmail.com>
2023-11-25 19:31:01 -0500

    Ajustes menores

--- modelo_clasificacion/model/pipeline.py
+++ modelo_clasificacion/model/pipeline.py
@@ -3,9 +3,8 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.multioutput import MultiOutputClassifier
 
 from model.config.core import config
-from model.processing import features as pp
 ## PIPELINE COMPLETO
-
+print(config.model_config.max_features)
 matriz_pipeline = Pipeline([("Random Forest",MultiOutputClassifier(
     RandomForestClassifier(max_features=config.model_config.max_features,
                            n_estimators=config.model_config.n_estimators,
